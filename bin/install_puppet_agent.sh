fd37cb9da4dcf22ce4ac725062e2a645e9ec762d
bentis <ben@tisdall.org.uk>
2013-05-30 10:05:55 +0100

    First commit (NO J#)

---
+++ bin/install_puppet_agent.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+### This script installs Puppet agent and facter.
+
+if [[ "$(whoami)" != "root" ]]; then
+  echo "Must be run as root"
+  exit
+fi
+
+PUPPET_VERSION=${PUPPET_VERSION:-"2.7.21-1puppetlabs1"}
+FACTER_VERSION=${FACTER_VERSION:-"1.7.0-1puppetlabs1"}
+INSTALL_PUPPET=0
+
+have_version(){
+  local PACKAGE=$1
+  local VERSION=$2
+  if [[ "$(dpkg-query -W -f='${Version}' $PACKAGE)" == "$VERSION" ]] && [[ "$(dpkg-query -W -f='${Status}' $PACKAGE|perl -lane 'print $F[-1]')" == "installed" ]]; then
+    return 0
+  fi
+  return 1
+}
+
+have_version puppet $PUPPET_VERSION || INSTALL_PUPPET=1
+have_version puppet-common $PUPPET_VERSION || INSTALL_PUPPET=1
+have_version facter $FACTER_VERSION || INSTALL_PUPPET=1
+
+if [[ "$INSTALL_PUPPET" == "1" ]]; then
+  (
+    TMPDIR=$(mktemp -d -p /tmp puppetinstall.XXXXXXXX)
+    cd $TMPDIR
+    apt-get -y purge $(dpkg -l|grep puppet|awk '{print $2}')
+    apt-get -y install wget
+    DEB="puppetlabs-release-$(lsb_release -c -s).deb"
+    wget -q http://apt.puppetlabs.com/$DEB -O $DEB
+    dpkg -i $DEB
+    apt-get update
+    apt-get -y install facter=$FACTER_VERSION puppet=$PUPPET_VERSION puppet-common=$PUPPET_VERSION subversion vim-puppet=$PUPPET_VERSION
+  )
+fi
