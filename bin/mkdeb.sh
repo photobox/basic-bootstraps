9aac466e8a72f329ef0e2a621cdd085fdb2ed5d9
bentis <ben@tisdall.org.uk>
2014-01-02 18:42:21 +0100

    Add more params

--- bin/mkdeb.sh
+++ bin/mkdeb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export PATH=$PATH:/var/lib/gems/1.8/gems/fpm-1.0.1/bin
 
-PREFIX=${PREFIX:-"/opt"}
 VERSION=${VERSION:-"1.0"}
 PREFIX=${PREFIX:-"/opt"}
 TYPE="deb"
 SOURCE="dir"
+BASE_NAME=${JOB_NAME#"build-"}
+fpm -C build -t $TYPE -s $SOURCE -n $BASE_NAME -v $VERSION --prefix $PREFIX $1
 
-fpm -t $TYPE -s $SOURCE -n $NAME -v $VERSION --prefix $PREFIX $1
