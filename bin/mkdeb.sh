e31f3b19598f9dee9bd4c1af00184cc631b9b149
bentis <ben@tisdall.org.uk>
2014-01-03 16:24:06 +0100

    use a temp dir to output deb into. Hard code '.' as file dir

--- bin/mkdeb.sh
+++ bin/mkdeb.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+set -e
 export PATH=$PATH:/var/lib/gems/1.8/gems/fpm-1.0.1/bin
 
 VERSION=${VERSION:-"1.0"}
-CHDIR=${CHDIR:-"build"}
+PAYLOAD_DIR=${PAYLOAD_DIR:-"build"}
+PAYLOAD_DIR="../$PAYLOAD_DIR"
+SCRIPTS_DIR=${SCRIPTS_DIR:-"package-scripts"}
+SCRIPTS_DIR="../$SCRIPTS_DIR"
 TYPE="deb"
 SOURCE="dir"
-BASE_NAME=${JOB_NAME#"build-"}
+NAME=${JOB_NAME#"build-"}
 EMAIL="Photobox Core Team <core@photobox.com>"
 URL="http://www.photobox.com"
 VENDOR="Photobox"
-SCRIPTS_DIR="./package-scripts"
+TMPDIR=$(mktemp -p . -d deb.XXXXXXXXXX)
+
+cd $TMPDIR
 
 [ -f "$SCRIPTS_DIR/postinst" ] && SCRIPTS+="--post-install $SCRIPTS_DIR/postinst "
 [ -f "$SCRIPTS_DIR/postrm" ] && SCRIPTS+="--post-uninstall $SCRIPTS_DIR/postrm "
 [ -f "$SCRIPTS_DIR/preinst" ] && SCRIPTS+="--before-install $SCRIPTS_DIR/preinst "
 [ -f "$SCRIPTS_DIR/prerm" ] && SCRIPTS+="--before-uninstall $SCRIPTS_DIR/prerm "
 
-. ./package-scripts/control.sh
+. $SCRIPTS_DIR/control.sh
 
+fpm -C $PAYLOAD_DIR -t $TYPE -s $SOURCE -n $NAME -v $VERSION --prefix $PACKAGE_PREFIX $DEPENDS $RECOMMENDS $SCRIPTS --description "$DESCRIPTION" -m "$EMAIL" --vendor $VENDOR --url $URL .
+
+scp *.deb proj.photobox.co.uk:/tmp
 rm *.deb
-fpm -C $CHDIR -t $TYPE -s $SOURCE -n $BASE_NAME -v $VERSION --prefix $PACKAGE_PREFIX $DEPENDS $RECOMMENDS $SCRIPTS --description "$DESCRIPTION" -m "$EMAIL" --vendor $VENDOR --url $URL $1
+cd ..
+rmdir $TMPDIR
