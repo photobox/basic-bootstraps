ac7da4857acb6516a3b7f9ba71aeab53abcc4bd5
bentis <ben@tisdall.org.uk>
2014-01-03 11:23:42 +0100

    Add things

--- bin/mkdeb.sh
+++ bin/mkdeb.sh
@@ -2,7 +2,6 @@
 export PATH=$PATH:/var/lib/gems/1.8/gems/fpm-1.0.1/bin
 
 VERSION=${VERSION:-"1.0"}
-PREFIX=${PREFIX:-"/opt"}
 CHDIR=${CHDIR:-"build"}
 TYPE="deb"
 SOURCE="dir"
@@ -20,4 +19,4 @@ SCRIPTS_DIR="./package-scripts"
 . ./package-scripts/control.sh
 
 rm *.deb
-fpm -C $CHDIR -t $TYPE -s $SOURCE -n $BASE_NAME -v $VERSION --prefix $PREFIX $DEPENDS $RECOMMENDS $SCRIPTS --description "$DESCRIPTION" -m "$EMAIL" --vendor $VENDOR --url $URL $1
+fpm -C $CHDIR -t $TYPE -s $SOURCE -n $BASE_NAME -v $VERSION --prefix $PACKAGE_PREFIX $DEPENDS $RECOMMENDS $SCRIPTS --description "$DESCRIPTION" -m "$EMAIL" --vendor $VENDOR --url $URL $1
