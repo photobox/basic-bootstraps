f25c367a6946d4d4d64f08326706617fc1d8c518
bentis <ben@tisdall.org.uk>
2014-01-05 21:55:43 +0100

    Implement proper upload command to >= 1 repos

--- bin/mkdeb.sh
+++ bin/mkdeb.sh
@@ -25,7 +25,17 @@ cd $TMPDIR
 
 fpm -C $PAYLOAD_DIR -t $TYPE -s $SOURCE -n $PACKAGE_NAME -v $VERSION --prefix $PACKAGE_PREFIX $DEPENDS $RECOMMENDS $SCRIPTS --description "$DESCRIPTION" -m "$EMAIL" --vendor $VENDOR --url $URL $FPM_EXTRA_FLAGS .
 
-scp *.deb proj.photobox.co.uk:/tmp
+REPO_HOST=${REPO_HOST:-"proj.photobox.co.uk"}
+BASE_REPO_PATH=${BASE_REPO_PATH:-"/install/repo/apt"}
+REPO_INJECT_COMMAND=${REPO_INJECT_COMMAND:-"/handsfree/scripts/debrepo_simple.pl"}
+
+for release in $UBUNTU_RELEASES; do
+  REPO_PATH="$BASE_REPO_PATH/$release"
+  scp *.deb $REPO_HOST:$REPO_PATH/binary
+  [ "$release" == "lucid" ] && ssh $REPO_HOST /handsfree/scripts/purge_debs.pl -v
+  ssh $REPO_HOST $REPO_INJECT_COMMAND -r $REPO_PATH -d binary
+done
+
 rm *.deb
 cd ..
 rmdir $TMPDIR
