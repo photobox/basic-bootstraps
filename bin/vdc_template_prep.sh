763c1132d5a06542bef4ecc8b49d21972aaf53c8
bentis <ben@tisdall.org.uk>
2014-10-14 14:40:56 +0100

    Make bootstrapper bootstrapper less clever

--- bin/vdc_template_prep.sh
+++ bin/vdc_template_prep.sh
@@ -2,28 +2,22 @@
 #
 set -e
 
-xlog(){
-  echo "$(date -u +%FT%T.%6NZ) [vdc_nethack] $1"
-  #logger -t vdc_nethack $1
-}
-
 export DEBIAN_FRONTEND=noninteractive
-
-[ -n "${GITHUB_USER}" ] || { echo "GITHUB_USER unset"; exit 1; }
-[ -n "${GITHUB_PASSWORD}" ] || { echo "GITHUB_PASSWORD unset"; exit 1; }
 
 wget -q -O- --no-check-certificate https://raw.githubusercontent.com/photobox/basic-bootstraps/master/bin/install_puppet_agent.sh | bash
 
 GIT="git-core"
 [[ "$(lsb_release -rs)" > "10.04" ]] && GIT="git"
 
-apt-get -y install $GIT
+apt-get -qy install $GIT
 
-exit
+cat <<EOF
 
-WORKDIR=$(mktemp -d)
-cd $WORKDIR
+---------------------------------------------------------------
+Prequisites installed, now run:
 
-git clone https://${GITHUB_USER}:${GITHUB_PASSWORD}@github.com/photobox/babel_vdc_provisioning.git
-
-puppet apply --modulepath modules default.pp
+  git clone git@github.com:photobox/babel_vdc_provisioning.git
+  cd babel_vdc_provisioning
+  sudo ./bin/install_bootstraps
+---------------------------------------------------------------
+EOF
